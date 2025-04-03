c456ecbf0f8d06407f5a9d013060adb65356eca1
linzhe141 <1572213544@qq.com>
2025-04-03 11:36:44 +0800

    feat: complete the 3*3 case

--- tests/debugger.spec.ts
+++ tests/debugger.spec.ts
@@ -14,3 +14,27 @@ test('debugger 2*2', () => {
 	count1(2);
 	count2(200);
 });
+
+test('debugger 3*3', () => {
+	const count1 = signal(1);
+	const count2 = signal(100);
+	const count3 = signal(1000);
+	effect(function fn1() {
+		console.log(`effect1-> count1 is: ${count1()}`);
+		console.log(`effect1-> count2 is: ${count2()}`);
+		console.log(`effect1-> count3 is: ${count3()}`);
+	});
+	effect(function fn2() {
+		console.log(`effect2-> count1 is: ${count1()}`);
+		console.log(`effect2-> count2 is: ${count2()}`);
+		console.log(`effect2-> count3 is: ${count3()}`);
+	});
+	effect(function fn3() {
+		console.log(`effect3-> count1 is: ${count1()}`);
+		console.log(`effect3-> count2 is: ${count2()}`);
+		console.log(`effect3-> count3 is: ${count3()}`);
+	});
+	count1(2);
+	count2(200);
+	count3(2000);
+});
